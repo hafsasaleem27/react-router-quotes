5c667eaefb0a60cb5ba0e188ea55a47436724e1b
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-10-14 15:48:28 +0500

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/quotes">
+
+      </Route>
+      <Route path="/new-quote">
+
+      </Route>
+      <Route path="/quotes/:quoteId">
+
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
