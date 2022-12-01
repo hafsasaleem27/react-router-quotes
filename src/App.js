47b524eae712f33ebb48a54934528f0df0313ee8
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-12-01 23:53:22 +0500

    added browser router in the app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetails";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/quotes">
-        
-      </Route>
-      <Route path="/new-quote">
-
-      </Route>
-      <Route path="/quotes/:quoteId">
-
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
