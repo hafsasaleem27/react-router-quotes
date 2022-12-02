b8645dc252b6ef879a96c52db8db67f8f5212905
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-12-03 00:38:21 +0500

    modified newquote component

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,3 +1,11 @@
-const NewQuote = () => {};
+import QuoteForm from "../components/quotes/QuoteForm";
+
+const addQuoteHandler = (quoteData) => {
+  console.log(quoteData);
+};
+
+const NewQuote = () => {
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
+};
 
 export default NewQuote;
