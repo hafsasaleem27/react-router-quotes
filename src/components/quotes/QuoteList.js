f5ce9c70d04ffce4fbc6b10a73e250828021c563
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-12-04 21:03:09 +0500

    extract query params and change button label / toggle params based on that

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,21 +1,27 @@
 import { Fragment } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
   const history = useHistory();
+  const location = useLocation();
+
+  console.log(location);
+
+  const queryParams = new URLSearchParams(location.search);
+  const isSortAscending = queryParams.get('sort') === 'asc';
 
   const sortingChangeHandler = () => {
-    history.push('/quotes?sort=asc');
+    history.push('/quotes?sort=' + (isSortAscending ? 'desc' : 'asc'));
   };
 
   return (
     <Fragment>
       <div className={classes.sorting}>
         <button onClick={sortingChangeHandler}>
-          Sort Ascending
+          Sort {isSortAscending ? 'Descending' : 'Ascending'}
         </button>
       </div>
       <ul className={classes.list}>
