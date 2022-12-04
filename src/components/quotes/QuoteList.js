7b302f3230652c4aab5003475d7e39863a8e3a20
Hafsa Saleem <hafsa.saleem27@gmail.com>
2022-12-04 20:41:08 +0500

    placed sort ascending button which appends query params upon click

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+
+  const sortingChangeHandler = () => {
+    history.push('/quotes?sort=asc');
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortingChangeHandler}>
+          Sort Ascending
+        </button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
